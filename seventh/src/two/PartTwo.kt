5e91d99eead76194af13dda2c9411f7d30a66706
Sondre Søberg <sondre.soberg@sikt.no>
2023-12-07 13:38:00 +0100

    feat(task-07): add solution for 7th of December

---
+++ seventh/src/two/PartTwo.kt
@@ -0,0 +1,120 @@
+package two
+
+import INPUT
+
+import two.Hand.Companion.toHands
+
+enum class Card(val value: String) {
+    A("A"),
+    K("K"),
+    Q("Q"),
+    T("T"),
+    NINE("9"),
+    EIGHT("8"),
+    SEVEN("7"),
+    SIX("6"),
+    FIVE("5"),
+    FOUR("4"),
+    THREE("3"),
+    TWO("2"),
+    J("J")
+}
+
+enum class HandValue {
+    FIVE_OF_A_KIND,
+    FOUR_OF_A_KIND,
+    FULL_HOUSE,
+    THREE_OF_A_KIND,
+    TWO_PAIR,
+    ONE_PAIR,
+    HIGH_CARD,
+}
+
+data class Hand(
+    private val value: String,
+    val bid: Int,
+) : Comparable<Hand> {
+    private val cardsInHand = getHand()
+
+    private fun getHand(): List<Card> = value.mapNotNull { value ->
+        Card.entries.find { it.value == value.toString() }
+    }
+
+    private fun hasTwoPairs(valuesPerKind: Map<String, List<Card>>): Boolean = valuesPerKind
+        .entries
+        .filter { it.value.size == 2 }
+        .size == 2
+
+    private fun valueOfHand(): HandValue {
+        val perKind = cardsInHand
+            .groupBy { it.value }
+
+        val numJs = perKind
+            .getOrDefault(Card.J.name, emptyList())
+            .size
+
+        val perKindNoJs = perKind.filterKeys { it != Card.J.name }
+
+        return when {
+            perKindNoJs.values.any { it.size + numJs == 5 } || numJs == 5 -> HandValue.FIVE_OF_A_KIND
+            perKindNoJs.values.any { it.size + numJs == 4  } -> HandValue.FOUR_OF_A_KIND
+            (perKind.values.any { it.size == 3 } && perKind.values.any { it.size == 2 }) ||
+                    (numJs == 1 && perKindNoJs.values.filter { it.size == 2 }.size == 2) -> HandValue.FULL_HOUSE
+            perKindNoJs.values.any { it.size + numJs == 3 } -> HandValue.THREE_OF_A_KIND
+            numJs == 0 && hasTwoPairs(perKind) -> HandValue.TWO_PAIR
+            perKindNoJs.values.any { it.size + numJs == 2 } -> HandValue.ONE_PAIR
+            else -> HandValue.HIGH_CARD
+        }
+    }
+
+    override fun compareTo(other: Hand): Int {
+        val value = valueOfHand()
+        val otherValue = other.valueOfHand()
+
+        if (value == otherValue) {
+            cardsInHand.forEachIndexed { i, card ->
+                if (card.ordinal > other.cardsInHand[i].ordinal) {
+                    return -1
+                }
+
+                if (card.ordinal < other.cardsInHand[i].ordinal) {
+                    return 1
+                }
+            }
+
+            return 0
+        }
+
+        return if (value.ordinal > otherValue.ordinal) -1 else 1
+    }
+
+    companion object {
+        fun String.toHands(): List<Hand> = split("\n")
+            .map { it.toHand() }
+
+        private fun String.toHand(): Hand = trim()
+            .split(" ")
+            .let {
+                Hand(
+                    value = it[0],
+                    bid = it[1].toInt()
+                )
+            }
+    }
+}
+
+class HandHandler(input: String) {
+    private val hands = input.toHands()
+
+    fun multiplyWithBid(): Int = sortedByValue()
+        .mapIndexed { i, hand -> hand.bid * (i + 1) }
+        .sum()
+
+    private fun sortedByValue(): List<Hand> =
+        hands.sorted()
+}
+
+fun main() {
+    val handHandler = HandHandler(INPUT)
+    println(handHandler.multiplyWithBid())
+}
