839202f3ad30af9e73d8a3e6652112f2e1361edb
Sondre Søberg <sondre.soberg@sikt.no>
2023-12-09 13:49:23 +0100

    feat(task-09): add solution for 9th of December

---
+++ ninth/src/one/PartOne.kt
@@ -0,0 +1,39 @@
+package one
+
+import INPUT
+
+data class Rows(
+    val nums: List<List<Long>>
+) {
+    constructor(input: String) : this(input.toNums())
+
+    fun getDifferences(): Long = nums.sumOf(::getDifferenceToNext)
+
+    companion object {
+        fun String.toNums(): List<List<Long>> = split("\n")
+            .map { numString ->
+                numString
+                    .trim()
+                    .split(" ")
+                    .map(String::toLong)
+            }
+
+        fun getDifferenceToNext(nums: List<Long>): Long = when {
+            nums.all { it == 0L } -> 0L
+            else -> {
+                val nextList = buildList {
+                    for (i in 1..<nums.size) {
+                        add(nums[i] - nums[i - 1])
+                    }
+                }
+
+                nums.last() + getDifferenceToNext(nextList)
+            }
+        }
+    }
+}
+
+fun main() {
+    val rows = Rows(INPUT)
+    println(rows.getDifferences())
+}
