5eff9a39545fba3ab54bbc638e15e08055f551b0
Sondre Søberg <sondre.soberg@sikt.no>
2024-12-19 12:48:10 +0100

    feat(2024): add solution for day 19

---
+++ 2024/src/19/part-two.ts
@@ -0,0 +1,38 @@
+import { INPUT_19 } from "@/src/19/input";
+
+const input = INPUT_19.split("\n\n");
+const keywords = input[0].split(", ");
+const searchStrings = input[1].split("\n");
+
+const countPossible = (searchString: string, keywords: string[]): number => {
+  const size = searchString.length;
+  const count = Array.from({ length: size + 1 }, () => 0);
+  count[0] = 1;
+
+  for (let i = 1; i < size + 1; i++) {
+    for (const keyword of keywords) {
+      const keywordSize = keyword.length;
+
+      if (
+        i >= keywordSize &&
+        searchString.slice(i - keywordSize, i) === keyword
+      ) {
+        count[i] += count[i - keywordSize];
+      }
+    }
+  }
+
+  return count[size];
+};
+
+const countAll = (): number => {
+  let result = 0;
+
+  for (const searchString of searchStrings) {
+    result += countPossible(searchString, keywords);
+  }
+
+  return result;
+};
+
+console.log(countAll());
